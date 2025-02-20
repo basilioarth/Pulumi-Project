6dfafee3f417d46cae45fb5fbd01874be7e72d3a
basilioarth <basilio.arth@gmail.com>
2025-02-20 17:29:42 -0300

    feat: Versão Inicial

---
+++ index.ts
@@ -0,0 +1,11 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
+
+// Create an AWS resource (S3 Bucket)
+const bucket = new aws.s3.BucketV2("pulumi-ftr");
+
+// Export the name of the bucket
+export const bucketName = bucket.id;
+export const bucketInfo = bucket.bucket;
+export const bucketArn = bucket.arn;
