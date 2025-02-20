eb30519795814526f2d53cdf8cc5a563d915e6db
basilioarth <basilio.arth@gmail.com>
2025-02-20 19:51:02 -0300

    feat: Criando o Primeiro Recurso

--- index.ts
+++ index.ts
@@ -3,7 +3,12 @@ import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.BucketV2("pulumi-ftr");
+const bucket = new aws.s3.BucketV2("pulumi-ftr", {
+    bucket: "pulumi-ftr",
+    tags: {
+        IAC: "true",
+    }
+});
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
