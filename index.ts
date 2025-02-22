816ca44cd2cb3fb33a75b4ce8769a4ac19e54a13
basilioarth <basilio.arth@gmail.com>
2025-02-22 14:03:48 -0300

    feat: Criando um Repositório ECR

--- index.ts
+++ index.ts
@@ -2,15 +2,28 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.BucketV2("pulumi-ftr", {
-    bucket: "pulumi-ftr",
+// Create an AWS S3 Bucket resource
+const bucket = new aws.s3.BucketV2("pulumi-ftr-bucket", {
+    bucket: "pulumi-ftr-bucket",
     tags: {
         IAC: "true",
     }
 });
 
-// Export the name of the bucket
+// Export infos from bucket
 export const bucketName = bucket.id;
 export const bucketInfo = bucket.bucket;
 export const bucketArn = bucket.arn;
+
+// Create an AWS ECR resource
+const ecr = new aws.ecr.Repository("pulumi-ftr-ecr", {
+    name: "pulumi-ftr-ecr",
+    imageTagMutability: "IMMUTABLE",
+    tags: {
+        IAC: "true",
+    }
+})
+
+// Export infos from ecr
+export const ecrName = ecr.name;
+export const ecrrepositoryUrl = ecr.repositoryUrl;
